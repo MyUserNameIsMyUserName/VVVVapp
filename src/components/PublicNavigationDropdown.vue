fc17465b9b30144f039196b1f49796367e7ea83e
MyUserNameIsMyUserName <slavko.vuletic92@gmail.com>
2020-12-05 07:26:05 +0100

    init2

---
+++ src/components/PublicNavigationDropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Pages
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :href="item.href"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PublicNavigationDropdown",
+    data: () => ({
+      items: [
+        { title: 'Home' , href:'/'},
+        { title: 'About' , href:'/about'}
+      ],
+    }),
+  }
+</script>
